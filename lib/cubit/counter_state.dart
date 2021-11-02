c71e7dca8ba8981f1b9c1b5dc1c9c01780e959f4
Fady Fawzy <FadyFawzy.Official@gmail.com>
2021-11-02 23:11:03 +0200

    Modify counter_state.dart to only include CounterState class for all possible states

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -1,9 +1,20 @@
 part of 'counter_cubit.dart';
 
-@immutable
-abstract class CounterState {}
-
 // From here, I will start by coding an implementation that will be more
 // extensive and self-explanatory instead of a quicker and abastract one.
 // I'm doning this so that you can understand all the concepts really well.
-class CounterInitial extends CounterState {}
+
+// This class will be the blueprint for all possible states which will be emitted
+// by the Cubit.
+class CounterState {
+  // Since the class must have the counter value, let's write it down and also
+  // create a constructor for it.
+  int counterValue;
+
+  // The application must have a vaild counter value at all times, so every time
+  // will have a new state emerging from the cubilt.
+  // The counter value must be a required attribute.
+  CounterState({
+    required this.counterValue,
+  });
+}
