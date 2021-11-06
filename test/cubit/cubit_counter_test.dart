a91d0936748900a533dc66ebdf0a67a9e099e424
Fady Fawzy <FadyFawzy.Official@gmail.com>
2021-11-06 11:13:28 +0200

    Understand the Structure of Testing (tests)

---
+++ test/cubit/cubit_counter_test.dart
@@ -0,0 +1,10 @@
+//* Inside our project, we need to create a spearate test folder (directory) in
+//* which we'll have all our test files.
+//! A little hint I can give you that every feature of your application needs
+//! to be tested in the first place.
+//* So the test inside the test folder sould be kind of symmetrical to the
+//* features from within your app.
+//* So all we have to do is to create the folder symmetrical to the 'lib' folder
+//* inside the 'test' folder and add '_test' to the name of the file.
+//! counter_cubit.dart -> counter_cubit_test.dart
+//* In which wil test out the functionality of our CounterCubit feature.
