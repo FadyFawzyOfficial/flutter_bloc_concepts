aa48987feec381dea79aae21e233c0a5d028e5d4
Fady Fawzy <FadyFawzy.Official@gmail.com>
2021-11-19 16:59:39 +0200

    Build a SecondScreen & Refactor with the Structure (Hierarchy) of Project Folders and Code to BLoC Architecture (Layers)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'cubit/counter_cubit.dart';
+import 'logic/cubit/counter_cubit.dart';
+import 'presentation/screens/home_screen.dart';
 
 void main() {
   //* Now, whenever we will compare CounterState, Dart will compare them
@@ -22,155 +23,21 @@ class MyApp extends StatelessWidget {
     // There is where the tree starts from in most of the flutter apps.
     // So inorder to make our CounterCubit available throughout the entire tree
     // over app, we must create our BlocProvider with it at our MaterialApp level.
-    return BlocProvider<CounterCubit>(
-      // All we need to do is to take the current context and return a
-      // CounterCubit instatnce.
-      //* So what we did now is we told Flutter to create a single & unique
-      //* instance of the CounterCubit and make it available to the subtree
-      //* below MaterialApp widget.
-      create: (context) => CounterCubit(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            //* BlocConsumer is a widget that combines both BlocListener &
-            //* BlocConsumer into a single widget.
-            // So instead of writing BlocListener on top of BlocBuilder as we did
-            // in our example, actually we can refactor the code by writing both
-            // of them inside the BlocComnsumer widget.
-            //* The code is now more readable and easier to maintain and the
-            //* application works prefect.
-            BlocConsumer<CounterCubit, CounterState>(
-              listener: (context, state) {
-                //* Based on our state.wasIncremented attribute, we will display a
-                //* SnackBar accordingly to whether the counter value of our
-                //* Counter State was incremented (state.wasIncremented == true)
-                //* or decremented (state.wasIncremented == false).
-                if (state.wasIncremented!) {
-                  Scaffold.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Incremented'),
-                      duration: Duration(microseconds: 500),
-                    ),
-                  );
-                } else if (!state.wasIncremented!) {
-                  Scaffold.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Decremented'),
-                      duration: Duration(microseconds: 500),
-                    ),
-                  );
-                }
-              },
-              // Now, Into the builder function, for every new emitted counter state
-              // (CounterState), the Text widget will show a new value.
-              // The value can be accessed by calling 'state.counterValue'.
-              builder: (context, state) {
-                // I can check whether the state's counterValue is negative
-                // and print a negative text widget.
-                if (state.counterValue < 0) {
-                  return Text(
-                    'BRR, NEGATIVE ${state.counterValue}',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                }
-                // I can check if the counterValue is even and print YAY
-                else if (state.counterValue % 2 == 0) {
-                  return Text(
-                    'YAAY ${state.counterValue}',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                }
-                // Or I can check if the counterValue is equal to five and print
-                // HMM, NUMBER 5
-                else if (state.counterValue == 5) {
-                  return Text(
-                    'HMM, NUMBER 5 ${state.counterValue}',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                } else {
-                  return Text(
-                    '${state.counterValue}',
-                    style: Theme.of(context).textTheme.headline4,
-                  );
-                }
-              },
-            ),
-            const SizedBox(height: 24),
-            // The 2 FloatingActionButton which are supposed to increment or
-            // decrement the counter.
-            // Remember that for every interaction an user makes with the app,
-            // there should be a state emerging from the app letting the user
-            // know what's going on?!
-            // Having that in mind, we conclude that for every tap of either of
-            // these 2 buttons, there should be a new counter state (CounterState),
-            // perhaps counter value (counterValue) shown on the screen.
-            // To do that, we need to call the increment or decrement functions
-            // on the cubit instance.
-            //? But, How do we access the cubit instance?!
-            //* Every time we press one of these buttons, the cubit, the increment
-            //* or decrement function will be called adding or subtracting 1 to
-            //* the current state.
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                // 2nd: So what happens when we press either of these 2 buttons,
-                // The increment or decrement function from inside the cubit,
-                // it gets called
-                //? What do these functions do?
-                FloatingActionButton(
-                  child: const Icon(Icons.remove_rounded),
-                  tooltip: 'Decrement',
-                  heroTag: Text(widget.title),
-                  onPressed: () {
-                    //* Either by calling
-                    BlocProvider.of<CounterCubit>(context).decrement();
-                    //* or with in
-                    // context.bloc<CounterCubit>().decrement();
-                  },
-                ),
-                FloatingActionButton(
-                  child: const Icon(Icons.add_rounded),
-                  tooltip: 'Increment',
-                  heroTag: Text('${widget.title} #2'),
-                  onPressed: () {
-                    BlocProvider.of<CounterCubit>(context).increment();
-                    // context.bloc<CounterCubit>().increment();
-                  },
-                ),
-              ],
-            ),
-          ],
+    // All we need to do is to take the current context and return a
+    // CounterCubit instatnce.
+    //* So what we did now is we told Flutter to create a single & unique
+    //* instance of the CounterCubit and make it available to the subtree
+    //* below MaterialApp widget.
+    //! To Learn the BLoC Access Concept we will move the BlocProvider to be the
+    //! parent widget of HomeScreen (Widget) instead of MaterialApp (Widget).
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: BlocProvider<CounterCubit>(
+        create: (context) => CounterCubit(),
+        child: const HomeScreen(
+          title: 'Flutter Demo Home Page',
+          color: Colors.blueAccent,
         ),
       ),
     );
