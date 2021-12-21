68ea9ced0e7dc0754bfcdafcac4549481f69b871
Fady Fawzy <FadyFawzy.Official@gmail.com>
2021-12-21 14:10:34 +0200

    Manage the SettingsScreen states by SettingsCubit & Link them (UI & Cubit) together

--- lib/presentation/screens/settings_screen.dart
+++ lib/presentation/screens/settings_screen.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../logic/cubit/settings_cubit.dart';
 
 // Settings Screen containing 2 SwitchListTile: App Notification & Email Notification.
-class SettingsScreen extends StatefulWidget {
+class SettingsScreen extends StatelessWidget {
   static const name = '/settings';
 
   const SettingsScreen({Key? key}) : super(key: key);
-
-  @override
-  State<SettingsScreen> createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
-  var appNotification = false;
-  var emailNotification = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,27 +16,40 @@ class _SettingsScreenState extends State<SettingsScreen> {
         title: const Text('Settings'),
         backgroundColor: Colors.grey[700],
       ),
-      body: Column(
-        children: [
-          SwitchListTile(
-            value: appNotification,
-            onChanged: (value) {
-              setState(() {
-                appNotification = value;
-              });
-            },
-            title: const Text('App Notifications'),
-          ),
-          SwitchListTile(
-            value: emailNotification,
-            onChanged: (value) {
-              setState(() {
-                emailNotification = value;
-              });
-            },
-            title: const Text('Email Notifications'),
-          ),
-        ],
+      //! We have also added a BlocListener to show a SnackBar to see the values.
+      body: BlocListener<SettingsCubit, SettingsState>(
+        listener: (context, state) {
+          final notificationSnackBar = SnackBar(
+            duration: const Duration(milliseconds: 500),
+            content: Text(
+                'App ${state.appNotification.toString().toUpperCase()}, '
+                'Email ${state.emailNotification.toString().toUpperCase()}'),
+          );
+
+          Scaffold.of(context).showSnackBar(notificationSnackBar);
+        },
+        //! We use BlocBuilder in order to rebuild the SwithListTile according
+        //! to the specific values found inside the SettingsState
+        child: BlocBuilder<SettingsCubit, SettingsState>(
+          builder: (context, state) {
+            return Column(
+              children: [
+                SwitchListTile(
+                  title: const Text('App Notifications'),
+                  value: state.appNotification,
+                  onChanged:
+                      context.read<SettingsCubit>().toggleAppNotification,
+                ),
+                SwitchListTile(
+                  title: const Text('Email Notifications'),
+                  value: state.emailNotification,
+                  onChanged:
+                      context.read<SettingsCubit>().toggleEmailNotification,
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
